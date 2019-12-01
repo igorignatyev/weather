c76dc48cb741a5a9e8b51316b096c861688af3eb
IgnatyevIS <ignatyev.i.s@sberbank.ru>
2019-12-01 22:07:32 +0300

    deleting some unnecessary stuff

--- target/test-classes/ru/ignatev/weather/controller/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO city_weather (name, last_sync, status, temp) VALUES
-('Miami,US', '2019-12-01 21:27:06.689', 'ACTIVE', 27.5),
-('Ukhta', '2019-12-01 21:27:06.689', 'ACTIVE', -7.5),
-('Moscow', '2019-12-01 21:27:06.689', 'ACTIVE', 2.6);
